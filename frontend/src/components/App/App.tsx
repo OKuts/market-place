2a6da5c6ea2af494284a45261c911ffc7128de59
Ol_Kuts <kuts.ol.vas@gmail.com>
2022-06-23 19:56:58 +0300

    begin

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import st from './App.module.scss'
+import { FC } from 'react'
+
+export const App: FC = () => {
+    return (
+        <div className={st.app}>
+            <h1>Hello</h1>
+        </div>
+    )
+}
